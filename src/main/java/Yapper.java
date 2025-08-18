ed56151dadc587e392215297b2635a0ad476e063
Bacen <ywj.ethan@gmail.com>
2025-08-18 21:58:38 +0800

    feat: add / print list

--- src/main/java/Yapper.java
+++ src/main/java/Yapper.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 /**
  * The Yapper class is the Chatbot for CS2103T.
  */
 public class Yapper {
     private Scanner inputScanner;
+    private ArrayList<String> tasks = new ArrayList<>();
 
     /**
      * Constructs a new Yapper chatbot instance.
@@ -24,6 +26,17 @@ public class Yapper {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
+    private void appendToList(String input) {
+        tasks.add(input);
+        System.out.printf("added: %s\n", input);
+    }
+
+    private void printList() {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.printf("%d. %s\n", i + 1, tasks.get(i));
+        }
+    }
+
     private void listenForCommand() {
         while (true) {
             String command = inputScanner.nextLine();
@@ -31,8 +44,10 @@ public class Yapper {
             if (command.equals("bye")) {
                 printExit();
                 break;
+            } else if (command.equals("list")) {
+                printList();
             } else {
-                System.out.println(command);
+                appendToList(command);
             }
         }
     }
