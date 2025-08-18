d2720237d4edd0382995cd6628b5d5806fddba78
Bacen <ywj.ethan@gmail.com>
2025-08-18 21:44:39 +0800

    feat: echo and exit on 'bye'

--- src/main/java/Yapper.java
+++ src/main/java/Yapper.java
@@ -1,18 +1,43 @@
+import java.util.Scanner;
 /**
  * The Yapper class is the Chatbot for CS2103T.
  */
 public class Yapper {
-    public static void main(String[] args) {
+    private Scanner inputScanner;
+
+    /**
+     * Constructs a new Yapper chatbot instance.
+     */
+    public Yapper() {
+        inputScanner = new Scanner(System.in);
         printGreet();
-        printExit();
+        listenForCommand();
     }
 
-    private static void printGreet() {
+    // Chatbot Behaviour
+    private void printGreet() {
         System.out.println("Hello! I'm Yapper");
         System.out.println("What can I do for you?");
     }
 
-    private static void printExit() {
+    private void printExit() {
         System.out.println("Bye. Hope to see you again soon!");
     }
+
+    private void listenForCommand() {
+        while (true) {
+            String command = inputScanner.nextLine();
+
+            if (command.equals("bye")) {
+                printExit();
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Yapper yapper = new Yapper();
+    }
+
 }
